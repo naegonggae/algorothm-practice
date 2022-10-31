9cd790c6412da51db4ae94baf3a538ee5c3e7a76
naegonggae <https://github.com/naegonggae/>
2022-10-31 09:43:46 +0900

    모의고사 문제 기초틀 구성

---
+++ src/main/java/com/likelion/mathtest/Solution.java
@@ -0,0 +1,14 @@
+package com.likelion.mathtest;
+
+public class Solution {
+    /*
+1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+     */
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+
+}
