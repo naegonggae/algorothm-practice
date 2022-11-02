927f9fbe5fd0355c179fa18ed3fc6ad9a4591f37
naegonggae <https://github.com/naegonggae/>
2022-11-02 09:39:08 +0900

    에라토스테네스 체 50이하 소수구하기

---
+++ src/main/java/com/likelion/primenumber/Eratos.java
@@ -0,0 +1,20 @@
+package com.likelion.primenumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Eratos {
+    public void findPNum(int number) {
+        List<Integer> num = new ArrayList<>;
+
+        for (int i = 2 ; i < 50 ; i++) {
+            // 2를 제외한 2의 배수를 지우는 로직
+            System.out.println(num.size());
+        }
+    }
+
+    public static void main(String[] args) {
+        Eratos eratos = new Eratos();
+        eratos.findPNum(50);
+    }
+}
